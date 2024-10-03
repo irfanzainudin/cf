85300c833709996a510ac583f6a274ed2991cbfc
Irfan Zainudin <m.irfan.zain@gmail.com>
2024-10-03 09:12:39 +0800

    working on 1578g to see if I can do it

---
+++ [WIP]-1578g.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cctype>
+#include <string>
+
+using namespace std;
+
+int main() {
+  int k, n, w;
+  cin >> k >> n >> w;
+
+  cout << k << ' ' << n << ' ' << w << '\n';
+}
